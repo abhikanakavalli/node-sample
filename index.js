c532fe7e0a6c130837494bdc249cee0a69d9040c
abhi kanakavalli <85701834+abhikanakavalli@users.noreply.github.com>
2023-05-14 06:26:16 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
-const express = require('express')
-const app = express()
-const port = 4000
+const express = require("express");
+const app = express();
+const port = 4000;
 
 app.get("/", (req, res) => {
-    res.status(200).send("<h1>Hello Abhi welcome to server!</h1>")
-})
+  res.status(200).send("<h1>Hello Abhi welcome to server!</h1>");
+});
+//parameter and bugs in route
+app.get("/api/v1/:token", (req, res) => {
+  console.log(req.params.token);
+  res.status(200).json({ param: req.params.token });
+});
 
-app.get('/api/v1/insta', (req, res) => {
-    res.status(200).json({
-        user: 'abhishek kanakavalli',
-        role: 'Frontend Developer',
-        experience: '1.5 years',
-        date: Date.now()
-    })
-})
+app.get("/api/v1/insta", (req, res) => {
+  res.status(200).json({
+    user: "abhishek kanakavalli",
+    role: "Frontend Developer",
+    experience: "1.5",
+    date: Date.now(),
+  });
+});
 
 app.listen(port, () => {
-    console.log(`Server is running on ${port}`)
-})
+  console.log(`Server is running on ${port}`);
+});
