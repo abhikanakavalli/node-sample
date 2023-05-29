09b70fdc9eb8d412980f77f67f5d01fbe1a0c231
abhikanakavalli <abhikanakavalli98@gmail.com>
2023-05-29 23:35:31 +0530

    check render fisrt

--- index.js
+++ index.js
@@ -3,17 +3,22 @@ const app = express()
 const port = 4000
 
 app.get("/", (req, res) => {
-    res.status(200).send("<h1>Hello Abhi welcome to server!</h1>")
+    res.status(200).send("<h1>Hello Abhi Kankavalli welcome to server!</h1>")
 })
 
 app.get('/api/v1/insta', (req, res) => {
     res.status(200).json({
         user: 'abhishek kanakavalli',
-        role: 'Frontend Developer',
+        role: 'Frontend Developers',
         experience: '1.5 years',
         date: Date.now()
     })
 })
+
+// app.get("api/v1/:token", (req, res) => {
+//     console.log(req.params.token)
+//     res.status(200).json({param: req.params.token})
+// }) 
 
 app.listen(port, () => {
     console.log(`Server is running on ${port}`)
